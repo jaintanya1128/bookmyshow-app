4ad77553a8bcbdc64da0381ef8bfa479e5b491d7
Tanya Jain <tjain12@sapient.com>
2019-11-22 23:31:02 +0530

    removed cypress

--- client/cypress/integration/test.spec.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="Cypress" />
-
-describe('My First Test', function() {
-	beforeEach(function() {
-		cy.visit('https://youtube.com');
-		cy.get('#search').type('kid carnival');
-	});
-	it('finds the content "type"', function() {});
-});
